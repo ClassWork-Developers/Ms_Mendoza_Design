ce9769cd4ddc931cd4360cfdf2fb9fc501a76f4a
Aderauwu04 <aderabg1@gmail.com>
2022-11-02 17:48:08 -0400

    Iniciación del proyecto

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to MAURICIO MENDOZA DESIGN"/>
+    <p>aaaaaaa</p>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#app {
+   p {
+    color: red;
+  }
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
