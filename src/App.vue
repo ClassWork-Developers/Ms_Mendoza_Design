8664ca7525e634f7bfa74a4674d4c0e8acfcd0fd
Aderauwu04 <aderabg1@gmail.com>
2022-11-02 18:05:36 -0400

    Eliminado caracteres de prueba

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to MAURICIO MENDOZA DESIGN"/>
-    <p>aaaaaaa</p>
   </div>
 </template>
 
@@ -19,9 +18,6 @@ export default {
 
 <style lang="scss" scoped>
 #app {
-   p {
-    color: red;
-  }
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
