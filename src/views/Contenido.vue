5a7beb164390d6ccf66d926fd1e3fa2973b33200
Aderauwu04 <aderabg1@gmail.com>
2022-11-27 13:37:06 -0400

    agregadas imagenes del contenido

--- src/views/Contenido.vue
+++ src/views/Contenido.vue
@@ -1,6 +1,10 @@
 <template>
   <section>
-    
+    <div v-for="(image, index) in images" :key="index">
+      <div>
+        <img :src="image.url" alt="">
+      </div>
+    </div>
   </section>
 </template>
 
@@ -8,6 +12,52 @@
 export default {
   name: 'PrototypeContent',
   props: {
-  }
+  },
+  data() {
+    return {
+      images: [
+        {
+          tittle : '',
+          url: '../assets/images/content/image_1.png',
+          description: ''
+        },
+        {
+          tittle : '',
+          url: '../assets/images/content/image_2.png',
+          description: ''
+        },
+        {
+          tittle : '',
+          url: '../assets/images/content/image_3.png',
+          description: ''
+        },
+        {
+          tittle : '',
+          url: '../assets/images/content/image_4.png',
+          description: ''
+        },
+        {
+          tittle : '',
+          url: "src/assets/images/content/image_5.png",
+          description: ''
+        },
+        {
+          tittle : '',
+          url: '../assets/images/content/image_6.png',
+          description: ''
+        },
+        {
+          tittle : '',
+          url: '/src/assets/images/content/image_7.png',
+          description: ''
+        },
+        {
+          tittle : '',
+          url: '../../src/assets/images/content/image_8.png',
+          description: ''
+        }
+      ]
+    }
+  },
 }
 </script>
