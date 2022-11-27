5f6dcfb530153bc616679ca61353ce3733d08c3c
Aderauwu04 <aderabg1@gmail.com>
2022-11-27 11:47:16 -0400

    Terminadas clases de los inputs

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="d-flex justify-content-center container">
-    <div class="col-4">
+    <div class="col-4 mx-2">
       <div class="d-flex column justify-content-center">
         <div class="d-flex align-items-center my-3">
           <div class="col-4">
@@ -39,23 +39,23 @@
         </div>
       </div>
     </div>
-    <div class="col-4">
-      <div class="form d-flex column">
-        <p class="h5">Formulario de contacto</p>
-        <div class="input-text">
-          <input type="text" id="name">
+    <div class="col-4 mx-2">
+      <div class="form">
+        <p class="h5 align-center py-2">Formulario de contacto</p>
+        <div class="input-text d-flex">
+          <input type="text" id="name" class="col-12">
           <label for="name">Nombre y apellido</label>
         </div>
-        <div class="input-text">
-          <input type="text" id="correo">
+        <div class="input-text d-flex">
+          <input type="text" id="correo" class="col-12">
           <label for="correo">Correo</label>
         </div>
-        <div class="input-text">
-          <input type="tel" id="telefono">
+        <div class="input-text d-flex">
+          <input type="tel" id="telefono" class="col-12">
           <label for="telefono">Teléfono</label>
         </div>
-        <div class="input-text">
-          <textarea id="asunto" cols="30" rows="10"></textarea>
+        <div class="input-text d-flex">
+          <textarea id="asunto" cols="30" rows="10" class="col-12"></textarea>
           <label for="asunto">Asunto</label>
         </div>
       </div>
